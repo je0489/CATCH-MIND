7fc26120061df87586a144cacd0245df79ebe7d9
je0489 <je0489@naver.com>
2021-09-06 22:23:42 +0900

    Set up and init

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 It's REALTIME DRAWING GRAM built with socketIO, gulp and nodeJS
 
 ## TODO
--  [x] server
--  [ ] pug
--  [ ] static
--  [ ] socketIO
+
+- [x] server
+- [x] pug
+- [x] static
+- [**x**] ESLint
+- [x] socketIO
--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
+import {
+    join
+} from "path";
 import express from "express";
+import socketIO from "socket.io";
 
 const PORT = 4000;
 const app = express();
 
-const listening = () => console.log( `http://localhost:${PORT}` );
-app.listen( PORT, listening() );
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+app.get("/", (req, res) => res.render("home"));
+
+const listening = () => console.log(`http://localhost:${PORT}`);
+const server = app.listen(PORT, listening());
+const io = socketIO(server);
