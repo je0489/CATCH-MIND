7b6e3ae04f5770629b76affe733ae1bc1f94328c
je0489 <je0489@naver.com>
2021-09-11 21:12:06 +0900

    Add a chat function using SocketIO

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-hello
+const socket = io("/");
+
+function sendMessage(message) {
+    socket.emit("newMessage", {
+        message
+    });
+}
+
+function setNickname(nickname) {
+    socket.emit("setNickname", {
+        nickname
+    });
+}
+
+function handleMessageNotify(data) {
+    const {
+        message,
+        nickname
+    } = data;
+    console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotify", handleMessageNotify);
--- src/server.js
+++ src/server.js
@@ -3,15 +3,30 @@ import {
 } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
-app.use(express.static(join(__dirname, "static")));
+app.use(logger("dev"));
+app.use(express.static(join(__dirname, "js")));
 app.get("/", (req, res) => res.render("home"));
 
 const listening = () => console.log(`http://localhost:${PORT}`);
 const server = app.listen(PORT, listening());
 const io = socketIO(server);
+
+// 소켓 연결시 실행
+io.on("connection", socket => {
+    socket.on("newMessage", ({
+        message
+    }) => socket.broadcast.emit("messageNotify", {
+        message,
+        nickname: socket.nickname || "NONE"
+    }));
+    socket.on("setNickname", ({
+        nickname
+    }) => socket.nickname = nickname)
+});
