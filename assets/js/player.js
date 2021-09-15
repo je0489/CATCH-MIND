ffb56798ffc6dc46b52031987085b1102fa263f1
je0489 <je0489@naver.com>
2021-09-16 01:05:18 +0900

    feat: add players score board

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,3 +1,14 @@
+const board = document.getElementById("jsPBoard");
+
+const addPlayers = players => {
+    board.innerHTML = "";
+    players.forEach(player => {
+        const playerElement = document.createElement("span");
+        playerElement.innerText = `${player.nickname}: ${player.points}`;
+        board.appendChild(playerElement);
+    })
+}
+
 export const handlePlayerUpdate = ({
     sockets
-}) => console.log(sockets);
+}) => addPlayers(sockets);
